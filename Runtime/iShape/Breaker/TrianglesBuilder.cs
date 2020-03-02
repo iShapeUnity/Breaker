4496e118e66c9207ebdf4a68b0b891925f446ac8
Nail Sharipov <nailxsharipov@gmail.com>
2020-03-02 17:25:31 +0300

    add new api

--- Runtime/iShape/Breaker/TrianglesBuilder.cs
+++ Runtime/iShape/Breaker/TrianglesBuilder.cs
@@ -1,4 +1,5 @@
 ﻿
+using iShape.Collections;
 using iShape.Geometry;
 using Unity.Collections;
 using UnityEngine;
@@ -6,6 +7,20 @@ using UnityEngine;
 public static class TrianglesBuilder {
     
     public static NativeArray<Triangle> build(NativeArray<Vector2> points, NativeArray<int> indices, Allocator allocator) {
+
+        int n = indices.Length / 3;
+        var triangles = new NativeArray<Triangle>(n, allocator);
+        for(int j = 0, i = 0; j < n; ++j, i += 3) {
+            var a = points[indices[i]];
+            var b = points[indices[i + 1]];
+            var c = points[indices[i + 2]];
+            triangles[j] = new Triangle(a, b, c);
+        }
+
+        return triangles;
+    }
+    
+    public static NativeArray<Triangle> build(NativeSlice<Vector2> points, NativeArray<int> indices, Allocator allocator) {
 
         int n = indices.Length / 3;
         var triangles = new NativeArray<Triangle>(n, allocator);
